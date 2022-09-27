560fa1e8e9bbfae4395007511ead8b574d90e64e
monmo538 <mona.moslemzadeh@gmail.com>
2022-09-27 17:49:03 +0200

    The latest updates was added

--- R/weather.forecast.R
+++ R/weather.forecast.R
@@ -1,7 +1,19 @@
-library(httr)
+library("httr")
+library("jsonlite")
+library("dplyr")
+library("ggplot2")
+library("lubridate")
+library("testthat")
 
+#' Title
+#' @param x R data frame.
+#' @param y A number.
+#' @return
+#' @export
+#'
+#' @examples
+#' 
 weather_forcast <-
   function() {
-    res <- VERB("GET", url = "https://api.tomorrow.io/v4/timelines?location=58.4108, 15.6214&fields=temperature&timesteps=1h&units=metric&apikey=rW9818R9IwR95rndf6JzGbWMnFkNsSTR")
-    cat(content(res, 'text'))
+   
   }
