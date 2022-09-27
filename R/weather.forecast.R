0b9c28e3f334e1ff1bc91fe808c850d7f574b4b7
monmo538 <mona.moslemzadeh@gmail.com>
2022-09-27 17:52:43 +0200

    The latest updates was added

--- R/weather.forecast.R
+++ R/weather.forecast.R
@@ -5,9 +5,9 @@ library("ggplot2")
 library("lubridate")
 library("testthat")
 
-#' Title
-#' @param x R data frame.
-#' @param y A number.
+#' @Title Get the weather information sources used between specified datetimes for a specified region.
+#' @param location R data frame.
+#' @param fields fields number.
 #' @return
 #' @export
 #'
@@ -15,5 +15,6 @@ library("testthat")
 #' 
 weather_forcast <-
   function() {
-   
+    res <- VERB("GET", url = "https://api.tomorrow.io/v4/timelines?location=58.4108, 15.6214&fields=temperature&timesteps=1h&units=metric&apikey=rW9818R9IwR95rndf6JzGbWMnFkNsSTR")
+    cat(content(res, 'text'))
   }
