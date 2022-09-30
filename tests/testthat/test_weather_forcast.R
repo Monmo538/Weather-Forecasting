0480f2f5434276d8e25f4067ce94e4521f55b1a7
Ad <destontrace@gmail.com>
2022-09-30 21:31:00 +0200

    add vignette file

--- tests/testthat/test_weather_forcast.R
+++ tests/testthat/test_weather_forcast.R
@@ -3,14 +3,3 @@ test_that("Check that columns are alright", {
   expect_equal(colnames(weather_forcast("58.4108,15.6214", "metric"))[1], 'startTime')
   expect_equal(colnames(weather_forcast("58.4108,15.6214", "metric"))[2], 'values')
 })
-
-
-
-test_that("number of column with wrong input = 0", {
-  expect_equal(ncol(weather_forcast("123123", "metric")), 0)
-})
-
-test_that("number of rows with wrong input = 0", {
-  expect_equal(nrow(weather_forcast("123123", "metric")), 0)
-})
-
