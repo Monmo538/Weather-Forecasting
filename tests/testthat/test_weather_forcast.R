866a8a9fbc2aa2de0262f5ef2d26233c193046a7
Ad <destontrace@gmail.com>
2022-11-05 16:47:03 +0100

    modeified test cases

--- tests/testthat/test_weather_forcast.R
+++ tests/testthat/test_weather_forcast.R
@@ -5,16 +5,16 @@ test_that("Check that columns are not alright", {
 })
 
 test_that("Check that invalid params throw errors", {
-  expect_error(weather_forcast("58.4108,15.6214", "metric2", "fakeAPIKey"))
-  expect_error(weather_forcast("58.4108,15.6214", "imperial2", "fakeAPIKey"))
+  expect_error(weather_forcast("58.4108,15.6214", "metric2", "fakeAPIKey"), "Invalid param weatherUnit")
+  expect_error(weather_forcast("58.4108,15.6214", "imperial2", "fakeAPIKey"), "Invalid param weatherUnit")
 })
 
 test_that("Check that missing params throw errors", {
-  expect_error(weather_forcast(location = "58.4108,15.6214"))
-  expect_error(weather_forcast(weatherUnit = "metric"))
-  expect_error(weather_forcast(apiKey = "fakeAPIKey"))
+  expect_error(weather_forcast(location = "58.4108,15.6214"), "Param missing")
+  expect_error(weather_forcast(weatherUnit = "metric"), "Param missing")
+  expect_error(weather_forcast(apiKey = "fakeAPIKey"), "Param missing")
 
-  expect_error(weather_forcast(location = "58.4108,15.6214", weatherUnit = "metric"))
-  expect_error(weather_forcast(apiKey = "fakeAPIKey", weatherUnit = "metric"))
-  expect_error(weather_forcast(apiKey = "fakeAPIKey", location = "58.4108,15.6214"))
+  expect_error(weather_forcast(location = "58.4108,15.6214", weatherUnit = "metric"), "Param missing")
+  expect_error(weather_forcast(apiKey = "fakeAPIKey", weatherUnit = "metric"), "Param missing")
+  expect_error(weather_forcast(apiKey = "fakeAPIKey", location = "58.4108,15.6214"), "Param missing")
 })
